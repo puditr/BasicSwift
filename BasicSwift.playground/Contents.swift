91f670dec70d1a3be184f086fdec06b6620a74ea
puditr <puditr@gmail.com>
2018-12-16 09:40:38 +0700

    Variable and Constant

---
+++ BasicSwift.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//Variable and Constant
+
+//Implicit การประกาศตัวแปรโดยใช้ Value เป็นตัวกำหนด Data Type
+//การประกาศแบบ Var คือ สามารแก้ไขคำได้
+var nameString = "Turbo"
+nameString = "Pudit"
